370373b44d7991deaf7db5fe41dded5b707baa6d
adilson <adilsonjcruz@live.com>
2025-03-08 14:36:50 -0300

    Acrescentando uma imagem do Mickey ao site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home(){
-    return <h1>Feliz dia Das Mulheres! Te Amo Muito!!!</h1>;
+    return (
+    <div>
+        <h1>My propourse is learning more for programing compute and help new students</h1>
+        <div>
+        <img src='/imgs/mickey.gif' alt="Mickey in the Concert!!!" />
+        </div>
+    </div>
+    );
 }
 
 export default Home;
