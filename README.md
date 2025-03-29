b091e38d8d641eb086beefa7bef2250cf4c64f87
adilson <adilsonjcruz@live.com>
2025-03-29 16:24:43 -0300

    ajustando a formatação dos arquivos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tab-news.com.br para o curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-function Home(){
-    return (
+function Home() {
+  return (
     <div>
-        <h1>My propourse is learning more for programing compute and help new students</h1>
-        <div>
-        <img src='/imgs/mickey.gif' alt="Mickey in the Concert!!!" />
-        </div>
+      <h1>
+        My propourse is learning more for programing compute and help new
+        students
+      </h1>
+      <div>
+        <img src="/imgs/mickey.gif" alt="Mickey in the Concert!!!" />
+      </div>
     </div>
-    );
+  );
 }
 
 export default Home;
